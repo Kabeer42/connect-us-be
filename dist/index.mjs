282d6245b1b888d634e567fcceaca2ab9e48bc81
Muhammad Kabeer <93073338+Kabeer42@users.noreply.github.com>
2025-10-31 12:35:37 +0500

    Update index.mjs

--- dist/index.mjs
+++ dist/index.mjs
@@ -36,8 +36,8 @@ const allowedOrigins = ['http://localhost:5173', 'http://192.168.100.12:5173', '
 //   }
 // });
 const io = new Server(server, {
+     path: "/socket.io",
     cors: {
-        path: "/socket.io",
         origin: "*", // Allow all origins
         methods: ["GET", "POST"],
         credentials: true, // you can keep this true if needed
